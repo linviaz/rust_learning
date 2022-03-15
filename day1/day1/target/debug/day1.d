5284072e9fe8998eb3e397235f6dee4fc3e0db48
root <mail.linzhang@gmail.com>
2022-03-15 11:00:21 +0100

    rm rust hw

--- day1/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/maill/Data/github/rust_learning/day1/day1/target/debug/day1: /mnt/c/Users/maill/Data/github/rust_learning/day1/day1/src/main.rs
--- day1/day1/target/debug/deps/day1-2636bc5d63400cc0.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/maill/Data/github/rust_learning/day1/day1/target/debug/deps/day1-2636bc5d63400cc0.rmeta: src/main.rs
-
-/mnt/c/Users/maill/Data/github/rust_learning/day1/day1/target/debug/deps/day1-2636bc5d63400cc0.d: src/main.rs
-
-src/main.rs:
