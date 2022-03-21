5b02dce53d736e31bae94aae75eb9bba6f83296e
Lin Zhang <mail.linzhang@gmail.com>
2022-03-21 13:51:00 +0100

    clean cached

--- day1/guessing_game/target/debug/deps/guessing_game-f9798fe6846083e9.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/maill/Data/github/rust_learning/day1/guessing_game/target/debug/deps/guessing_game-f9798fe6846083e9: src/main.rs
-
-/mnt/c/Users/maill/Data/github/rust_learning/day1/guessing_game/target/debug/deps/guessing_game-f9798fe6846083e9.d: src/main.rs
-
-src/main.rs:
--- day1/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/maill/Data/github/rust_learning/day1/guessing_game/target/debug/guessing_game: /mnt/c/Users/maill/Data/github/rust_learning/day1/guessing_game/src/main.rs
